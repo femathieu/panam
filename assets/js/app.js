3a13e82408c0a0e0dff5e15c6af0427e52d0104d
Pierre Sid-idris <pierre.sididris@live.fr>
2018-10-11 23:56:56 +0200

    menu integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,23 @@ require('bootstrap');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+        if (!$(this).next().hasClass('show')) {
+          $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+      
+      
+        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+          $('.dropdown-submenu .show').removeClass("show");
+        });
+      
+      
+        return false;
+      });
+
+    //   if( $('.print-item').hasClass('show') ) {
+    //     $(this).children('.second-level').addClass('.print-item-height');
+    //     }
 });
